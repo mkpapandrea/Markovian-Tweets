b29f50af652514b9abc0a6c129a65bc27c190562
Mary <mkpapandrea@gmail.com>
2020-03-09 15:46:52 -0400

    final version

--- markov_tweet.cpp
+++ markov_tweet.cpp
@@ -58,76 +58,67 @@ void add_word(string word, string follower){
     }
 }
 
-// Analyzes a single tweet (1 line from a text file)
-// This breaks up the tweet into base words and the word following it.
+
 void parse_line(string line){
-    size_t word_start_index = 0;
-    size_t word_end_index;
-    
-    size_t follower_start_index;
-    size_t follower_end_index;
-    
+    size_t word_start = 0;
+    size_t word_end;
+    size_t follower_start;
+    size_t follower_end;
     size_t word_len;
     size_t follower_len;
-    
-    string word = "";
+    string word = "";       // blank strings
     string follower = "";
-    
-    // Goes through all the words in the tweet
-    while(word_start_index != string::npos){
-        word_end_index = line.find(" ", word_start_index);
-        word_len = word_end_index - word_start_index;
-        
-        // Find the next base word
-        while(word_len == 0){
-            word_start_index++;
-            word_end_index = line.find(" ", word_start_index);
-            word_len = word_end_index - word_start_index;
+    ////////////////////////////////////////////////////////////////////////////////////
+    while(word_start != string::npos){      //npos description http://www.cplusplus.com/reference/string/string/npos/
+        word_end = line.find(" ", word_start);  // find spaces and switch indexes
+        word_len = word_end - word_start;
+       ///////////////////////////////////////////////////////////// 
+        while(word_len == 0){       // look for substring
+            word_start++;
+            word_end = line.find(" ", word_start);
+            word_len = word_end - word_start;
             
-            // Case where the last word in the tweet has been reached
-            if(word_start_index >= line.length()){
-                word_start_index = line.length(); // Should always be the length anyway due to logic of the find function, but set in case of boundary conditions 
+            if(word_start >= line.length()){
+                word_start = line.length(); 
                 break;
             }
         }
         
-        word = line.substr(word_start_index, word_len); // Now holds a single word from the tweet.
+        word = line.substr(word_start, word_len); 
         
-        // Now find the following word (if there is one)
-        if(word_end_index != string::npos){
-            follower_start_index = word_end_index + 1;
-            follower_end_index = line.find(" ", follower_start_index);
-            follower_len = follower_end_index - follower_start_index;
+        if(word_end != string::npos){       // looks for first substring after the end or word_end
+            follower_start = word_end + 1;
+            follower_end = line.find(" ", follower_start);
+            follower_len = follower_end - follower_start;
             
             while(follower_len == 0){
-                follower_start_index++;
-                follower_end_index = line.find(" ", follower_start_index);
-                follower_len = follower_end_index - follower_start_index;
-                if(follower_start_index >= line.length()){
-                    follower_start_index = line.length(); // should always be the length anyway due to logic of the find function, but set in case of boundary conditions 
+                follower_start++;
+                follower_end = line.find(" ", follower_start);
+                follower_len = follower_end- follower_start;
+                if(follower_start>= line.length()){
+                    follower_start= line.length();  
                     break;
                 }
             }
             
-            word = line.substr(word_start_index, word_len);
-            follower = line.substr(follower_start_index, follower_len); // Now contains the word right after the base word
-            
-            word_start_index = follower_start_index;    // The next base word will be the current base word's follower
+            word = line.substr(word_start, word_len);
+            follower = line.substr(follower_start, follower_len); 
+            word_start = follower_start;    
         }
         else{
-            follower = "";  // If the base word is that last word in the tweet, then it does not have a following word
-            word_start_index = string::npos;
+            follower = "";  //set as ending condition for words with no followers. in make_tweet if =0
+            word_start = string::npos;
         }
         
-        add_word(word, follower);   // Add the base word and its follower to the vector of Markov links
-                                    // or update the base word's vector of followers if there is already a record
+        add_word(word, follower);   
+                                    
     }
 }
 
 
 int parse_file(string filename){    //parses line by line. line is considered tweet so as to not fuck up senstences
     string line;
-    
+    cout<< "parse file"<< endl;
     ifstream myfile(filename.c_str());
     if (myfile.is_open()){
         while(getline (myfile,line)){
@@ -149,46 +140,50 @@ int get_rand_index(int choices){    //in a function cause it needs to repeatedly
 }
 
 
-void write_tweet(){
+void make_tweet(){         //random first word. in while it picks from followers then goes follower->word.
+    //cout<<"    in make_tweet"<< endl;
+    bool finished=false;
+    string tweet= "";
+    int follower_count;
+    int follower_index;
+
+    string wrd;
+    int index= get_rand_index(MC.size());
     
-    int word_index = get_rand_index(MC.size()); // Randomly select the first word of the tweet
-    int follower_index = 0;
-    int follower_num = 0;
-    
-    bool tweet_done = false;
-    string last_word = ""; // most recent word added, used to search vector
-    
-    tweet = "";
-    tweet = MC.at(word_index).word; // First word of the tweet
-    last_word = tweet;  // Since the tweet is only 1 word long right now, first word = last word
-    
-    while(!tweet_done){
-        word_index = get_index(last_word);
-        Word temp = MC.at(word_index);
-        
-        follower_num = temp.followers.size();
-        follower_index = get_rand_index(follower_num);  // Randomly select the next word. Because of word distribution in sentences,
-                                                        // the follower word, on average, should be the most frequent word seen after a given base word
-        last_word = temp.followers.at(follower_index);
-        
-        if(last_word.length() == 0){    // If the last "word" is an empty string, then the tweet is complete.
-            tweet_done = true;
+    tweet= MC.at(index).word;
+    wrd=tweet;
+
+
+    /////////////////////////////////////////////////////////////////////////
+    while(!finished){
+        index= get_index(wrd);
+        Word current= MC.at(index);
+        follower_count=current.followers.size();
+        follower_index=get_rand_index(follower_count);
+
+        wrd= current.followers.at(follower_index);
+        if(wrd.length()==0){        //if length =0, means not a word. no followers
+            finished=true;
         }
         else{
-            if(tweet.length() + last_word.length() > 279){ // If adding the next word makes the tweet longer than 279 characters, don't add it to the tweet.
-                tweet_done = true;
+             if(tweet.length()+ wrd.length()>280){      // doesnt go over 280
+                finished=true;
             }
             else{
-                tweet = tweet + " " + last_word;    // Add the next word to the tweet with a space between the words. (This space is why we use the 279 above, instead of 280)
-            }
+                tweet= tweet+" " +wrd;
+            }  
         }
     }
-    cout << tweet << endl;
+        
+    if(finished=true){
+        cout<< tweet<< endl;
+        }
+ 
 }
 
 void print_some_tweets(int amount){
     for(int i = 0; i < amount; i++){
-        write_tweet();
+        make_tweet();
         cout << endl;
     }
 }
@@ -200,11 +195,13 @@ void print_some_tweets(int amount){
 
 /////////////////////////////////////////main///////////////////////////////////////
 int main(int argc, char* argv[]){               // run by adding text file name after ./excname
+    //cout<< "being main";
     string arguement(argv[1]);
     srand(time(NULL));
 
 
     int amount;
+    //cout<< "look";
     if(parse_file(arguement)){
         cout<< " how many tweets yall want?  ";
         cin >> amount;
